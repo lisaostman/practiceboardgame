f5bc832554624dc1aedb3dd2aab33b71df39ef25
Lisa Ostman <peolina@live.com.au>
2022-09-01 17:54:06 +1000

    hides start menu when players click start

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let crewMember = 2;
 const crew = [];
+const crewPoints = [];
 
 //query selectors
 const form = document.querySelector("#crewForm");
 const inputContainer = document.querySelector("#inputContainer");
 const formButton = document.querySelector("#submitCrew");
 const readyButton = document.querySelector("#ready");
+const startMenu = document.querySelector("#start");
 
 // adding input field if more players should be added
 function addCrewMember(e) {
@@ -24,16 +26,24 @@ function addCrewMember(e) {
 
 // starting game function after user clicks ready
 function startGame(e) {
-// prevents page reloading by default
+  // prevents page reloading by default
   e.preventDefault();
-// for loop goes through the input elements inside the input container
+  // for loop goes through the input elements inside the input container
   for (let i = 0; i < inputContainer.children.length; i++) {
     console.log(inputContainer.children[i].value);
     // push each input element's value (name) into the crew array
     let crewName = inputContainer.children[i].value;
     crew.push(crewName);
+    // crew points will need as many points as crew members (input elements)
+    crewPoints.push(0);
   }
-  console.log(`Current crew consists of : ${crew}`);
+
+  console.log(
+    `Current crew consists of : ${crew} and their points are ${crewPoints}`
+  );
+
+  // hide the starting menu
+  startMenu.setAttribute("style", "display: none");
 }
 
 // adding click event to button so input field is added for a new crew member
