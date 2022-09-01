cf7bcf0da674556472ae5614ef042a5d246dd27d
Lisa Ostman <peolina@live.com.au>
2022-09-01 18:22:12 +1000

    added ability for prompts to be displayed and clicked through and a reset button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log(promptArray);
+
 let crewMember = 2;
 const crew = [];
 const crewPoints = [];
@@ -8,6 +10,10 @@ const inputContainer = document.querySelector("#inputContainer");
 const formButton = document.querySelector("#submitCrew");
 const readyButton = document.querySelector("#ready");
 const startMenu = document.querySelector("#start");
+const game = document.querySelector("#game");
+const promptContainer = document.querySelector("#promptContainer");
+const nextPrompt = document.querySelector("#nextPrompt");
+const reset = document.querySelector("#reset");
 
 // adding input field if more players should be added
 function addCrewMember(e) {
@@ -22,6 +28,14 @@ function addCrewMember(e) {
   newElement.setAttribute("name", "crew" + crewMember);
   // append the new input element to the end of the container of crew member input fields
   inputContainer.appendChild(newElement);
+}
+
+// function for setting up a question in the prompt box
+function nextQuestion() {
+  // finds number lower than promptArray.length but higher or equal to min (indeces)
+  let randomNumber = Math.floor(Math.random() * promptArray.length);
+  console.log(randomNumber);
+  promptContainer.innerHTML = promptArray[randomNumber];
 }
 
 // starting game function after user clicks ready
@@ -44,8 +58,16 @@ function startGame(e) {
 
   // hide the starting menu
   startMenu.setAttribute("style", "display: none");
+  game.setAttribute("style", "display: block");
+  nextQuestion();
+}
+
+function resetFn() {
+  location.reload();
 }
 
 // adding click event to button so input field is added for a new crew member
 formButton.addEventListener("click", addCrewMember);
 readyButton.addEventListener("click", startGame);
+nextPrompt.addEventListener("click", nextQuestion);
+reset.addEventListener("click", resetFn);
---
+++ prompts.js
@@ -0,0 +1,5 @@
+const promptArray = [
+  "If you didn't see the sun today, down your drink (you scurvy-ridden fuck), ",
+  "Crew members with a tattoo, give away a drink for each one.",
+  "Down your drink, ",
+];
